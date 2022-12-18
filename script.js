5f6ec2a898e20343bd71b36ae6755d378e56b644
Rohit <rohit.rgurung@gmail.com>
2022-12-18 13:25:53 +0545

    Add Backspace Button, Manage the display range

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function multiply(a,b)
 
 function divide(a,b)
 {
-    return +(a/b).toFixed(10);
+    return a/b;
 }
 
 function operate(a,b,operator)
@@ -40,21 +40,55 @@ const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const equals = document.querySelector('#equals');
 const clear = document.querySelector('#clear');
+const del = document.querySelector('#del');
 
 bottomText.textContent = 0;
 
+function truncateDisplay(num)
+{
+    let temp;
+    num = num.toString();
+    if(num.length <= 13)
+    {
+        if(num.includes('.') && num.indexOf('.') < 12)
+        {
+            temp = 12-num.indexOf('.');
+            num = +num;
+            return num.toFixed(temp);
+        }
+        return +num;
+    }
+    else
+    {
+        if(num.includes('.') && num.indexOf('.') < 12)
+        {
+            temp = 12-num.indexOf('.');
+            num = +num;
+            return num.toFixed(temp);
+        }
+        alert("Can't display the result because the result exceed the display range of numbers with 13 characters including '.'");
+        return undefined;
+    }
+}
+
 function display(event)
 {
-    if(bottomText.textContent.length < 13 && (secondNum === undefined || secondNum === undefined))
-    {
         if(symbol === undefined)
         {
             if(bottomText.textContent === '0')
             {
                 bottomText.textContent = '';
             }
-            bottomText.textContent += event.target.textContent;
-            firstNum = bottomText.textContent;
+            if(bottomText.textContent.length < 13)
+            {
+                bottomText.textContent += event.target.textContent;
+                firstNum = bottomText.textContent;
+            }
+            else
+            {
+                alert("Can't enter numbers more than 13 characters including '.'. Please delete some in order to continue the calculation.")
+            }
+            
         }
         else
         {
@@ -62,24 +96,29 @@ function display(event)
             {
                 bottomText.textContent = '';
             }
-            bottomText.textContent += event.target.textContent;
-            secondNum = bottomText.textContent;
+            if(bottomText.textContent.length < 13)
+            {
+                bottomText.textContent += event.target.textContent;
+                secondNum = bottomText.textContent;
+            }
+            else
+            {
+                alert("Can't enter numbers more than 13 characters including '.'. Please delete some in order to continue the calculation.")
+            }
         }
-    }
-    else
-    {
-        alert("Can't enter more digits. Please delete some digits to continue the calculation.");
-    }
 }
 
 function operateFunction(event)
 {
     if(secondNum !== undefined)
     {
-        firstNum = operate(+firstNum,+secondNum,symbol);
-        bottomText.textContent = firstNum;
+        if(truncateDisplay(operate(+firstNum,+secondNum,symbol)) !== undefined)
+        {
+            firstNum = operate(+firstNum,+secondNum,symbol);
+            bottomText.textContent = truncateDisplay(firstNum);
+            secondNum = undefined;
+        }
     }
-    secondNum = undefined;
     topText.textContent = '';
     topText.textContent += firstNum + ' ' + event.target.textContent;
     symbol = event.target.textContent;
@@ -89,12 +128,15 @@ function equalFunction(event)
 {
     if(firstNum !== undefined && secondNum !== undefined)
     {
-        topText.textContent = '';
-        topText.textContent += firstNum + ' ' + symbol + ' ' + secondNum + ' '  + event.target.textContent;
-        firstNum = operate(+firstNum,+secondNum,symbol);
-        bottomText.textContent = firstNum;
-        secondNum = undefined;
-        symbol = undefined;
+        if(truncateDisplay(operate(+firstNum,+secondNum,symbol))!== undefined)
+        {
+            topText.textContent = '';
+            topText.textContent += firstNum + ' ' + symbol + ' ' + secondNum + ' '  + event.target.textContent;
+            firstNum = operate(+firstNum,+secondNum,symbol);
+            bottomText.textContent = truncateDisplay(firstNum);
+            secondNum = undefined;
+            symbol = undefined;
+        }
     }
 }
 
@@ -107,7 +149,18 @@ function clearFunction(event)
     symbol = undefined;
 }
 
+function delFunction()
+{
+    bottomText.textContent = bottomText.textContent.substring(0,bottomText.textContent.length-1);
+    secondNum = bottomText.textContent;
+    if (bottomText.textContent === '')
+    {
+        bottomText.textContent = 0;
+    }
+}
+
 numbers.forEach(number => number.addEventListener('click', display));
 operators.forEach(operator => operator.addEventListener('click',operateFunction));
 equals.addEventListener('click',equalFunction);
 clear.addEventListener('click',clearFunction);
+del.addEventListener('click', delFunction);
